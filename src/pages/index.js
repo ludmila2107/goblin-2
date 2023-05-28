e7922d6bc07bff917061dd292adbfb93bcb8d61f
Ludmila <lvminzulina@mail.ru>
2023-05-28 11:47:32 +0300

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import "./index.css";
 import appearGoblin from "../components/appearGoblin.js";
-import clickCell from "../components/clickCell.js";
 import { field } from "../constants/constants.js";
 
 setInterval(appearGoblin, 1000);
